02d1d39fc6df13cd8ca3ab269c0abba716d69616
Medo20300 <medhatconcrete@gmail.com>
2024-09-03 16:13:20 +0300

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,50 +2,30 @@
 
 const request = require('request');
 
-// Get the movie ID from the command-line arguments
 const movieId = process.argv[2];
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-// Construct the URL for the movie endpoint
-const movieEndpoint = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-// Function to request and print character names
-function fetchCharacterNames(characterList, index) {
-  if (index >= characterList.length) {
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
     return;
   }
 
   request(characterList[index], (error, response, body) => {
     if (error) {
-      console.error(error);
-      return;
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
     }
-
-    try {
-      const character = JSON.parse(body);
-      console.log(character.name);
-    } catch (err) {
-      console.error('Failed to parse character data:', err);
-    }
-
-    // Fetch the next character
-    fetchCharacterNames(characterList, index + 1);
   });
 }
 
-// Request the movie details to get the list of characters
 request(movieEndpoint, (error, response, body) => {
   if (error) {
-    console.error(error);
-    return;
-  }
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
 
-  try {
-    const movie = JSON.parse(body);
-    const characterList = movie.characters;
-    
-    // Start fetching character names
-    fetchCharacterNames(characterList, 0);
-  } catch (err) {
-    console.error('Failed to parse movie data:', err);
+    sendRequest(characterList, 0);
   }
 });
